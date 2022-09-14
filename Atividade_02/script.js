63c5b51c9f98effc496f0e064bdf27f35de324bd
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-09-13 21:03:51 -0300

    feat: ajustes para separar arquivo html e js

---
+++ Atividade_02/script.js
@@ -0,0 +1,15 @@
+
+let cor_original_quadrado;
+
+window.onload = function(){
+    let quadrado = document.getElementById("quadrado");
+    cor_original_quadrado = quadrado.style.backgroundColor;
+}
+
+function retornar_cor_original() {
+    document.getElementById("quadrado").style.backgroundColor = cor_original_quadrado;
+}
+
+function mudar_cor_quadrado(){
+    document.getElementById("quadrado").style.backgroundColor = "rgb(0, 0, 0)";
+}
