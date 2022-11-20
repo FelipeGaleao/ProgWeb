d025618cda6f1346c526b815493e9ed3e0d10987
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-20 14:50:00 -0300

    feat: exibir os dados de csv

---
+++ Prova_02/app/Questao_4/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Ler o arquivo csv 
+$rows = array_map('str_getcsv', file('../Questao_3/lista.csv'));
+$header = array_shift($rows);
+$csv = array();
+foreach ($rows as $row) {
+    $csv[] = array_combine($header, $row);
+}
+
+foreach ($csv as $row) {
+    foreach ($row as $key => $value) {
+        echo $key . ' => ' . $value . '<br>';
+    }
+    echo '<hr>' . '<br>';
+}
+?>
